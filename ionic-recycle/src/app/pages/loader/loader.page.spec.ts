e3804a608e49b357cc17f73dc75b9fb6d505bc63
Joaquin E Islas <joa.islas@gmail.com>
2023-07-25 09:07:26 -0300

    including the firts navigation test

--- ionic-recycle/src/app/pages/loader/loader.page.spec.ts
+++ ionic-recycle/src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,36 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   }));
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should go to login page after load', () => {
+      spyOn(router,'navigate');
+     component.ngOnInit();
+     expect(router.navigate).toHaveBeenCalledWith(['login']);
   });
 });
